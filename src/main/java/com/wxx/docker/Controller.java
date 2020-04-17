b6905e16a31fc3a4a5b66782cdd0313804278f99
WeisonWei <weixx3@126.com>
2020-04-17 19:34:00 +0800

    update

--- src/main/java/com/wxx/docker/Controller.java
+++ src/main/java/com/wxx/docker/Controller.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @RestController
 @Slf4j
@@ -17,6 +19,7 @@ public class Controller {
 
     private static int num = 0;
 
+    private static final Lock lock = new ReentrantLock(true);
 
     @Autowired
     RedissonClient redissonClient;
@@ -26,41 +29,90 @@ public class Controller {
         return "hello";
     }
 
-
-    @GetMapping("/lock1")
-    public String test(@RequestParam String threadName) throws InterruptedException {
-        RLock lock = redissonClient.getFairLock("myLock");
-        boolean res = lock.tryLock(10, 20, TimeUnit.SECONDS);
-        while (res) {
-            try {
-                int i = num + 1;
-                setThreadName(threadName + i);
-                log.info(Thread.currentThread().getName() + "lock1:" + i);
-                TimeUnit.SECONDS.sleep(3);
-                return String.valueOf(i);
-            } finally {
-                lock.unlock();
-            }
-        }
-        return Thread.currentThread().getName() + "未获取锁";
+    @GetMapping("/locks/n")
+    public String noLock(@RequestParam String name) {
+        num = num + 1;
+        setThreadName(name + num);
+        log.info(Thread.currentThread().getName() + "NoLock:" + num);
+        return "hello";
     }
 
-    @GetMapping("/lock2")
-    public String test1(@RequestParam String name) throws InterruptedException {
-        RLock lock = redissonClient.getFairLock("myLock");
-        boolean res = lock.tryLock(10, 20, TimeUnit.SECONDS);
+    @GetMapping("/locks/s")
+    public synchronized String synchronizedLock(@RequestParam String name) {
+        long begin = System.currentTimeMillis();
+        num = num + 1;
+        setThreadName(name + num);
+        log.info(Thread.currentThread().getName() + "SynchronizedLock:" + num);
+        long end = System.currentTimeMillis();
+        return String.valueOf(num) + "cost:" + (end - begin) + "ms";
+    }
+
+    @GetMapping("/locks/r")
+    public String reentrantLockLock(@RequestParam String name) throws InterruptedException {
+        long begin = System.currentTimeMillis();
+        lock.lock();
+        try {
+            num = num + 1;
+            setThreadName(name + num);
+            log.info(Thread.currentThread().getName() + "ReentrantLockLock:" + num);
+        } finally {
+            lock.unlock();
+        }
+        long end = System.currentTimeMillis();
+        return String.valueOf(num) + "cost:" + (end - begin) + "ms";
+    }
+
+
+    @GetMapping("/locks/rl1")
+    public String redisRLock1(@RequestParam String name) throws InterruptedException {
+        long begin = System.currentTimeMillis();
+        int number = 0;
+        RLock lock = redissonClient.getFairLock(name);
+        boolean res = lock.tryLock(11, 11, TimeUnit.SECONDS);
         while (res) {
             try {
-                int i = num + 1;
-                setThreadName(name + i);
-                log.info(Thread.currentThread().getName() + "lock2:" + i);
-                TimeUnit.SECONDS.sleep(3);
-                return String.valueOf(i);
+                number = number + 1;
+                setThreadName(name + number);
+                log.info(Thread.currentThread().getName() + "lock1:" + number);
+                log.info(Thread.currentThread().getName() + "redisRLock1:" + number);
+                TimeUnit.SECONDS.sleep(6);
             } finally {
-                lock.unlock();
+                if (lock.isHeldByCurrentThread()) {
+                    lock.unlock();
+                }
+                res = false;
+                break;
             }
         }
-        return Thread.currentThread().getName() + "未获取锁";
+        long end = System.currentTimeMillis();
+        log.info(number + "cost:" + (end - begin) + "ms");
+        return number + "cost:" + (end - begin) + "ms";
+        //return Thread.currentThread().getName() + "未获取锁";
+    }
+
+    @GetMapping("/locks/rl2")
+    public String redisRLock2(@RequestParam String name) throws InterruptedException {
+        long begin = System.currentTimeMillis();
+        RLock lock = redissonClient.getFairLock("myLock");
+        boolean res = lock.tryLock(11, 11, TimeUnit.SECONDS);
+        while (res) {
+            try {
+                num = num + 1;
+                setThreadName(name + num);
+                log.info(Thread.currentThread().getName() + "redisRLock2:" + num);
+                TimeUnit.SECONDS.sleep(3);
+                return String.valueOf(num);
+            } finally {
+                if (lock.isHeldByCurrentThread()) {
+                    lock.unlock();
+                }
+                res = false;
+                break;
+            }
+        }
+        long end = System.currentTimeMillis();
+        log.info(num + "cost:" + (end - begin) + "ms");
+        return num + "cost:" + (end - begin) + "ms";
     }
 
     private void setThreadName(@RequestParam String name) {
